338be6d6a5c21b8d3370054c081174a3dde6ac54
leung <huolinliang@gmail.com>
2016-05-20 17:21:58 +0800

    add joke interface

---
+++ app/src/main/java/com/yan/haha/Jokes.java
@@ -0,0 +1,40 @@
+package com.yan.haha;
+
+
+public class Jokes {
+
+	private String title;
+	private String pubDate;
+	private String body;
+
+	public Jokes(String title, String pubDate, String body) {
+		super();
+		this.title = title;
+		this.pubDate = pubDate;
+		this.body = body;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPubDate() {
+		return pubDate;
+	}
+
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
---
+++ app/src/main/java/com/yan/haha/OnDataFinishedListener.java
@@ -0,0 +1,10 @@
+package com.yan.haha;
+
+/**
+ * Created by Leung on 2016/5/20.
+ */
+public interface OnDataFinishedListener {
+    public void onDataSuccessfully(Object data);
+
+    public void onDataFailed();
+}
