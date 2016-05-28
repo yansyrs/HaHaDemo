bd934d5724a0ef9e57540e288ae5f8e805163ad9
ysm <yansyrs@gmail.com>
2016-05-28 12:09:54 +0800

    ui modify

--- app/src/main/java/com/yan/haha/MainActivity.java
+++ app/src/main/java/com/yan/haha/MainActivity.java
@@ -82,8 +82,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_brain_riddles) {
+            // 脑筋急转弯
             replaceContentFragment(new BrainRiddleFragment());
         } else if (id == R.id.nav_gallery) {
 
